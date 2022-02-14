10cdd5f4f166d0fa7c4daaf987e22a74480a0995
Austin Cronin <austin.cronin3d@gmail.com>
2022-02-14 08:00:29 -0500

    added contact page skeleton

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,41 @@
 export default function contact () {
-    
+    /*
+        <div id="contact-wrapper">
+            // name
+            // email
+            // message
+            // social sidebar ? 
+            <div id="contact">
+            </div>
+            <div id="social">
+                <ol>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>TikTok</li>
+                </ol>
+                // social media
+            </div>
+        </div>
+    */
+
+    // TODO: build out contact interface
+    const element = document.createElement('div');
+    element.id = "contact-wrapper";
+    const contact = document.createElement('div');
+
+    // Add sidebar to include social media
+    // TODO: Add social images
+    const sidebar = document.createElement('div');
+    const sidebarList = document.createElement('ol');
+    const socialList = ["Facebook", "Instagram","TikTok"];
+
+    for (let i=0; i < socialList.length;i++){
+        let socialItem = document.createElement("li");
+        socialItem.innerHTML = socialList[i];
+        sidebarList.appendChild(socialItem);
+    }
+    sidebar.appendChild(sidebarList);
+
+
+    return element;
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import loadPage from "./load";
 
 loadPage();
+
+// add events to listen for tab switching
+// function to clear out the current content
+// add new content of the tab that was clicked
